b7de6223c1f2c1b2cb2b67cdbaaf2d39337c1183
Kateřina Holasová <katerina.holasova@artin.cz>
2023-09-30 10:26:03 +0200

    add categories

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,5 +1,15 @@
+export enum Category {
+  Crime = 'crime',
+  Fantasy = 'fantasy',
+  Fiction = 'fiction',
+  Horror = 'horror',
+  NonFiction = 'nonFiction',
+  ScienceFiction = 'scienceFiction',
+}
+
 export enum CoverImage {
   Animal = 'animal.jpg',
+  Beach = 'beach.jpg',
   City = 'city.jpg',
   Food = 'food.jpg',
   nature = 'nature.jpg',
@@ -8,6 +18,7 @@ export enum CoverImage {
 export type Book = {
   _id: string
   author: string
+  category: Category
   coverImage: CoverImage
   description?: string
   title: string
