57b2d1961913bbf5761300d31311ad8c8e34a176
Kateřina Holasová <katerina.holasova@artin.cz>
2023-09-30 20:59:22 +0200

    add booklist title

---
+++ src/app/books/BookListPage.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { BookList } from '@/components/BookList'
+import { BookListLoader } from '@/components/BookListLoader'
+import { Header } from '@/components/Header'
+import { LINKS } from '@/constants/links'
+import { useBookList } from '@/hooks/useBookList'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@mui/material'
+import Link from 'next/link'
+
+export default function BookListPage() {
+  const { data, isLoading } = useBookList()
+
+  return (
+    <>
+      <Header title="All books">
+        <Button component={Link} href={LINKS.newBook} startIcon={<FontAwesomeIcon icon={faPlus} />}>
+          Add a new book
+        </Button>
+      </Header>
+      {data && <BookList data={data} />}
+      {isLoading && <BookListLoader />}
+    </>
+  )
+}
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,26 +1,10 @@
-'use client'
-import { BookList } from '@/components/BookList'
-import { BookListLoader } from '@/components/BookListLoader'
-import { Header } from '@/components/Header'
-import { LINKS } from '@/constants/links'
-import { useBookList } from '@/hooks/useBookList'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button } from '@mui/material'
-import Link from 'next/link'
+import { Metadata } from 'next'
+import BookListPage from './BookListPage'
+
+export const metadata: Metadata = {
+  title: 'All books',
+}
 
 export default function Page() {
-  const { data, isLoading } = useBookList()
-
-  return (
-    <>
-      <Header title="All books">
-        <Button component={Link} href={LINKS.newBook} startIcon={<FontAwesomeIcon icon={faPlus} />}>
-          Add a new book
-        </Button>
-      </Header>
-      {data && <BookList data={data} />}
-      {isLoading && <BookListLoader />}
-    </>
-  )
+  return <BookListPage />
 }
