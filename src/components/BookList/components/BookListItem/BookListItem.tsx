f81247f3d0baab72c7bd4714dbbc122b866493d4
Kateřina Holasová <katerina.holasova@artin.cz>
2023-09-30 17:21:46 +0200

    BookListItem fixes

--- src/components/BookList/components/BookListItem/BookListItem.tsx
+++ src/components/BookList/components/BookListItem/BookListItem.tsx
@@ -1,4 +1,4 @@
-import { Box, ButtonBase, Stack, Typography } from '@mui/material'
+import { Box, ButtonBase, Stack, Theme, Typography, useMediaQuery } from '@mui/material'
 import { BookListItemProps } from './types'
 import { wrapperSx } from './styles'
 import { CoverImage } from '@/components/CoverImage'
@@ -10,6 +10,8 @@ import { LINKS } from '@/constants/links'
 export function BookListItem(props: BookListItemProps) {
   const { _id, author, category, coverImage, title } = props
   const actionMenuItems = useActionMenuItems(_id)
+
+  const isTabletOrDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
 
   return (
     <ButtonBase
@@ -26,11 +28,12 @@ export function BookListItem(props: BookListItemProps) {
         component="span"
         direction="row"
         justifyContent="space-between"
+        spacing={2}
       >
         <Typography component="span" variant="h4">
           {title}
         </Typography>
-        <ActionMenu items={actionMenuItems} />
+        {isTabletOrDesktop && <ActionMenu items={actionMenuItems} />}
       </Stack>
       <Typography component="span">{author}</Typography>
     </ButtonBase>
