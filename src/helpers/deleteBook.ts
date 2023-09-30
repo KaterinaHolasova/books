374aa5fb9a2966b39694159b94a4125e71f695b1
Kateřina Holasová <katerina.holasova@artin.cz>
2023-09-30 17:23:47 +0200

    refactor deleteBook

--- src/helpers/deleteBook.ts
+++ src/helpers/deleteBook.ts
@@ -2,7 +2,6 @@ import { API_URL } from '@/constants/apiUrl'
 
 export function deleteBook(id: string) {
   return fetch(API_URL.bookDetail(id), {
-    headers: { 'Content-Type': 'application/json; charset=utf-8' },
     method: 'DELETE',
   })
 }
