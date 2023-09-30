a02023f0241e6444a9e73b4956a884eca5d9b442
Kateřina Holasová <katerina.holasova@artin.cz>
2023-09-30 12:03:50 +0200

    add BookListItem hover styles

--- src/components/BookList/components/BookListItem/styles.ts
+++ src/components/BookList/components/BookListItem/styles.ts
@@ -1,6 +1,14 @@
 import { SxProps, Theme } from '@mui/material'
 
-export const wrapperSx: SxProps<Theme> = () => ({
+export const wrapperSx: SxProps<Theme> = (theme) => ({
   textAlign: 'left',
   width: '100%',
+
+  ['& img']: {
+    transition: theme.transitions.create('opacity'),
+  },
+
+  ['&:hover img']: {
+    opacity: 0.8,
+  },
 })
